8d6db61bc926238e25ce68c4adc3e553af0ac344
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2017-01-25 19:11:23 -0200

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'pybreaker',
-    version = '0.3.0',
+    version = '0.3.1',
     description = 'Python implementation of the Circuit Breaker pattern',
     long_description = open('README.rst', 'r').read(),
     keywords = ['design', 'pattern', 'circuit', 'breaker', 'integration'],
