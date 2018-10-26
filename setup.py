1f73314bb06daf1784e8fa8e85e41c89a11454c5
Daniel Martins <daniel.martins@descomplica.com.br>
2018-10-26 14:59:35 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pybreaker',
-    version='0.4.4',
+    version='0.4.5',
     description='Python implementation of the Circuit Breaker pattern',
     long_description=open('README.rst', 'r').read(),
     keywords=['design', 'pattern', 'circuit', 'breaker', 'integration'],
