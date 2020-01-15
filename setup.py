2ee7bba787a29f715805b038fb98eda767534f10
Daniel Martins <daniel.martins@jusbrasil.com.br>
2020-01-15 17:18:36 -0300

    Update classifiers to drop support for Python 2.7

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries',
     ],
